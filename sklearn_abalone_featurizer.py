57f0ce5e1a1758b8a0cf1a5b8d6570e3b782dc56
David Gibb <degibb@amazon.com>
2020-04-26 21:45:44 -0700

    Inference lookup working, need to do some cleanup

--- sklearn_abalone_featurizer.py
+++ sklearn_abalone_featurizer.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import time
 import sys
+import traceback
 from io import StringIO
 import os
 import shutil
@@ -17,6 +18,11 @@ from sklearn.externals import joblib
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import Binarizer, StandardScaler, OneHotEncoder
+
+import boto3
+import decimal
+
+from botocore.exceptions import ClientError
 
 from sagemaker_containers.beta.framework import (
     content_types, encoders, env, modules, transformer, worker)
@@ -44,7 +50,48 @@ feature_columns_dtype = {
     'viscera_weight': np.float64,
     'shell_weight': np.float64}
 
+split_1_feature_columns_names = [
+    'ID',
+    'sex', # M, F, and I (infant)
+    'length', # Longest shell measurement
+    'diameter', # perpendicular to length
+    'height' # with meat in shell
+] # after being dried
+
+split_2_feature_columns_names = [
+    'ID',
+    'whole_weight', # whole abalone
+    'shucked_weight', # weight of meat
+    'viscera_weight', # gut weight (after bleeding)
+    'shell_weight'] # after being dried
+
+split_1_feature_columns_dtype = {
+    'ID': np.int64,
+    'sex': str,
+    'length': np.float64,
+    'diameter': np.float64,
+    'height': np.float64}
+
+split_2_feature_columns_dtype = {
+    'ID': np.int64,
+    'whole_weight': np.float64,
+    'shucked_weight': np.float64,
+    'viscera_weight': np.float64,
+    'shell_weight': np.float64}
+
 label_column_dtype = {'rings': np.float64} # +1.5 gives the age in years
+
+dynamodb = boto3.resource("dynamodb", region_name='us-west-1')
+table = dynamodb.Table('feature_store')
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if abs(o) % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
 
 def merge_two_dicts(x, y):
     z = x.copy()   # start with x's keys and values
@@ -69,13 +116,7 @@ if __name__ == '__main__':
                           'This usually indicates that the channel ({}) was incorrectly specified,\n' +
                           'the data specification in S3 was incorrectly specified or the role specified\n' +
                           'does not have permission to access the data.').format(args.train, "train"))
-    
-    raw_data = [ pd.read_csv(
-        file, 
-        header=None, 
-        names=feature_columns_names + [label_column],
-        dtype=merge_two_dicts(feature_columns_dtype, label_column_dtype)) for file in input_files ]
-    concat_data = pd.concat(raw_data)
+   
     
     # This section is adapted from the scikit-learn example of using preprocessing pipelines:
     #
@@ -92,6 +133,14 @@ if __name__ == '__main__':
     # - shell_weight: Weight after being dried
     # Categorical Features:
     # - sex: categories encoded as strings {'M', 'F', 'I'} where 'I' is Infant
+    
+    raw_data = [ pd.read_csv(
+        file, 
+        header=None, 
+        names=feature_columns_names + [label_column],
+        dtype=merge_two_dicts(feature_columns_dtype, label_column_dtype)) for file in input_files ]
+    concat_data = pd.concat(raw_data)
+
     numeric_features = list(feature_columns_names)
     numeric_features.remove('sex')
     numeric_transformer = Pipeline(steps=[
@@ -114,6 +163,39 @@ if __name__ == '__main__':
     joblib.dump(preprocessor, os.path.join(args.model_dir, "model.joblib"))
 
     print("saved model!")
+
+def ddb_request_transform(item_ids):
+    keys = [{'ID': str(id)} for id in item_ids]
+    return {'Keys': keys}
+
+def load_and_merge_from_feature_store(inputdf):
+    responses = []
+    id_array = inputdf.ID.tolist()
+    batch_size = 25
+    id_array_batched = [id_array[i * batch_size:(i + 1) * batch_size] for i in range((len(id_array) + batch_size - 1) // batch_size )]
+    try:
+        for arr in id_array_batched:
+            response = dynamodb.batch_get_item(
+                RequestItems={
+                    'feature_store': ddb_request_transform(arr)
+                }
+            )
+            responses.extend(response['Responses']['feature_store'])
+            time.sleep(0.5)
+
+
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        #print(json.dumps(response['Responses']['feature_store'], indent=4, cls=DecimalEncoder))
+        #item = response['Item']
+        #print("GetItem succeeded:")
+        #print(json.dumps(item, indent=4, cls=DecimalEncoder))    
+        print(json.dumps(responses, indent=4, cls=DecimalEncoder))
+    print("responses", responses)
+    split_2_df = pd.DataFrame(responses, columns=split_2_feature_columns_names).astype(split_2_feature_columns_dtype)
+    joined_df = inputdf.merge(split_2_df, left_on='ID', right_on='ID').drop('ID', axis=1)
+    return joined_df
     
     
 def input_fn(input_data, content_type):
@@ -123,6 +205,7 @@ def input_fn(input_data, content_type):
     and unlabelled data we first determine whether the label column is present
     by looking at how many columns were provided.
     """
+    print("In input_fn")
     if content_type == 'text/csv':
         # Read the raw input data as CSV.
         df = pd.read_csv(StringIO(input_data), 
@@ -130,11 +213,20 @@ def input_fn(input_data, content_type):
         
         if len(df.columns) == len(feature_columns_names) + 1:
             # This is a labelled example, includes the ring label
+            print("Length indicates that label is included")
             df.columns = feature_columns_names + [label_column]
         elif len(df.columns) == len(feature_columns_names):
+            print("Length indicates that label is not included")
             # This is an unlabelled example.
             df.columns = feature_columns_names
+        elif len(df.columns) == len(split_1_feature_columns_names):
+            print("Length indicates that we should hit the feature store")
+            df.columns = split_1_feature_columns_names
+            split_1_df = df.astype(split_1_feature_columns_dtype)
+            print("incoming data", split_1_df.head())
+            df = load_and_merge_from_feature_store(split_1_df)
             
+        print("merged df", df.head())
         return df
     else:
         raise ValueError("{} not supported by script!".format(content_type))
